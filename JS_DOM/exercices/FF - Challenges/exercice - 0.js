e1ab08878a043e20d1f01436ff60f21b5509cc53
thierry <thierrypoupon@hotmail.com>
2020-03-05 15:24:22 +0100

    add files exercice et correction

---
+++ JS_DOM/exercices/FF - Challenges/exercice - 0.js
@@ -0,0 +1,14 @@
+/*
+	L'objectif de cet exercice est de créer une
+	animation dans un balise canvas. Il s'agit
+	d'animer un lever - couché de soleil sur une
+	plaine.
+
+	- Crééz une balise canvas;
+	- Créez un dessin qui représente le ciel;
+	- Créer un dessin qui représente le soleil;
+	- Créez un dessin qui représente votre plaine;
+	- Le soleil doit décrire un arc de cercle au dessus de la plaine;
+	- La luminosité ambiante doit varier en fonction de la hauteur
+	  du soleil dans l'espace de dessin.
+*/
